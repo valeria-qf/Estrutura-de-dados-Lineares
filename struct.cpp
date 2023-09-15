d8d8e505a45428911e840095cb738322f12b828c
Valéria Vitória de Queiroz Freire <20221094040025@ifrn.local>
2023-09-15 14:33:19 -0300

    first commit :tada:

---
+++ struct.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct Person
+{
+    int age;
+    char name[20];
+    float weight;
+}
+family_member;
+
+int main()
+{
+    Person sister;
+    Person brother;
+
+    sister.age = 10;
+    brother.age = 15;
+
+    cout << "Sister Age: " <<  sister.age << endl;
+    cout << "Brother Age: " <<  brother.age << endl;
+}
