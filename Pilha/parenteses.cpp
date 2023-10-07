54b6c5e2d3f5a1ce39e765e6591106e92d14c491
Valéria <vvqf2016@gmail.com>
2023-10-07 18:00:15 -0300

    added 'parenthesis challenge' with stacks

---
+++ Pilha/parenteses.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    char parenthesis;
+    Node *next;
+    Node(char parenthesis)
+    {
+        this->next = nullptr;
+        this->parenthesis = parenthesis;
+    }
+};
+
+class Stack
+{
+    private:
+    Node *head;
+
+    public:
+    Stack()
+    {
+        this->head = nullptr;
+    }
+
+    ~Stack()
+    {
+        while(!isEmpty())
+        {
+            pop();
+        }
+    }
+
+    bool isEmpty()
+    {
+        return head == nullptr;
+    }
+
+    void push(char parenthesis)
+    {
+        Node *newNode = new Node(parenthesis);
+        
+        if (isEmpty())
+        {
+            head = newNode;
+        }
+
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Pilha vazia" << endl;
+        }
+
+        else
+        {
+            Node *temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void verifyParenthesis(string expression)
+    {
+        for (int i = 0; i < expression.length(); i++)
+        {
+            if (expression[i] == '(')
+            {
+                push(expression[i]);
+            }
+
+            else if (expression[i] == ')')
+            {   
+                if (isEmpty())
+                {
+                    cout << "Nao ha correspondente para fechar" << endl;
+                    return; // finaliza for
+                }
+
+                else
+                {
+                    pop();
+                   
+                }
+                
+            }
+            
+            else
+            {
+                cout << "Caractere inválido!" << expression[i] << endl;            
+            }    
+        }
+
+        if (isEmpty())
+        {
+            cout << "Expressao valida" << endl;
+        }
+
+        else
+        {
+            cout << "Expressao invalida!" << endl;
+        }
+          
+    }
+
+    void print()
+    {
+        if(!isEmpty())
+        {
+            Node *current = head;
+            while (current != nullptr)
+            {
+                cout << current->parenthesis << " ";
+                current = current->next;
+            }
+            cout << endl;     
+        }
+    }
+};
+
+
+int main()
+{
+    Stack stack;
+    cout << "Insira uma expressao de parentesis" << endl;
+
+    string expression;
+    cin >> expression;
+    stack.verifyParenthesis(expression);
+    cout << endl;
+    stack.print();
+
+    return 0;
+}
